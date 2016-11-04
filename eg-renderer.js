3fb2b5f1c999601ddf9d971724f16e45adf78e0d
Yosuke ONOUE <onoue@likr-lab.com>
2016-11-04 16:19:32 +0900

    Updates

--- eg-renderer.js
+++ eg-renderer.js
@@ -9927,7 +9927,7 @@
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
 	var interpolate = function interpolate(current, next, r) {
-	  return r > 1 ? next : (next - current) * r + current;
+	  return (next - current) * r + current;
 	};
 
 	var interpolateVertex = function interpolateVertex(current, next, r) {
@@ -9975,6 +9975,9 @@
 	};
 
 	var interpolateLayout = exports.interpolateLayout = function interpolateLayout(current, next, r) {
+	  if (r > 1) {
+	    return next;
+	  }
 	  var vertices = Object.keys(next.vertices);
 	  var result = {
 	    vertices: {},
